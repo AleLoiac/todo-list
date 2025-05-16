ea70308ce7ed4772ad14caa725df599b0af1787e
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-05-16 11:55:48 +0200

    Add toggleCompletion and changePriority methods

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,13 +1,24 @@
 class ToDo {
     constructor(title, description, dueDate, priority) {
         this.title = title;
-        this.description = description;
+        this.description = description || "";
         this.dueDate = dueDate;
-        this.priority = priority;
+        this.priority = priority || "low";
         this.completion = false;
+    }
+
+    toggleCompletion() {
+        this.completion = !this.completion;
+    }
+
+    changePriority(value) {
+        this.priority = value;
     }
 }
 
-export function addTodo(name, description, dueDate, priority) {
-    return new ToDo(name, description, dueDate, priority);
+export function addTodo(title, description, dueDate, priority) {
+    if (!title) {
+        return
+    }
+    return new ToDo(title, description, dueDate, priority);
 }
