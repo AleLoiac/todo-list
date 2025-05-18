5a3dc60bb98ea3d89674371ce5f89d3ffe47adb4
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-05-18 15:46:11 +0200

    Fire project selection using event delegation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import "./styles.css";
 import { createProject } from "./modules/project";
-import { addProject } from "./modules/domUtils";
+import { addProject, selectProject } from "./modules/domUtils";
 import { handleProject } from "./modules/domUtils";
 
 createProject("Default project");
 addProject("Default project");
 
 const addProjectBtn = document.querySelector(".add-project-btn");
+const projectList = document.querySelector(".project-list");
 
 addProjectBtn.addEventListener("click", handleProject);
+
+projectList.addEventListener("click", (e) => {
+    if (e.target.classList.contains("project")) {
+        selectProject(e.target.textContent);
+    }
+})
