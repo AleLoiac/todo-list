71e8c6326c34a8814faf1cbe4c1922c76b89770c
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-05-18 13:00:41 +0200

    Add event listener to add project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import "./styles.css";
-import { addProject } from "./modules/project";
+import { createProject } from "./modules/project";
+import { addProject } from "./modules/domUtils";
+import { handleProject } from "./modules/domUtils";
 
-const defaultProject = addProject("default");
+createProject("Default project");
+addProject("Default project");
+
+const addProjectBtn = document.querySelector(".add-project-btn");
+
+addProjectBtn.addEventListener("click", handleProject);
