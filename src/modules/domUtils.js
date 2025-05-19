46a5228eaf6f1ce3ea05c69d527e404bdde13a9e
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-05-19 11:42:23 +0200

    Add todo selection function

--- src/modules/domUtils.js
+++ src/modules/domUtils.js
@@ -53,15 +53,16 @@ export function selectProject(name) {
         }
     }
     const todoList = currentProject.getTodoList();
+    const list = newElement("div", "todo-list", "", openProject);
 
     for (const todo of todoList) {
-        renderTodo(todo);
+        renderTodo(todo, list);
     }
 }
 
-function renderTodo(todo) {
-    const todoList = newElement("div", "todo-list", "", openProject)
-    const newTodo = newElement("div", "todo", "", todoList);
+function renderTodo(todo, parent) {
+    const newTodo = newElement("div", "todo", "", parent);
+    newTodo.dataset.id = todo.id;
     newElement("p", "", todo.title, newTodo);
     newElement("p", "", todo.dueDate, newTodo);
 }
@@ -72,4 +73,67 @@ function newElement(tag, className, textContent, parent) {
 	if (textContent) element.textContent = textContent;
 	parent.appendChild(element);
     return element
+}
+
+export function selectTodo(projectTitle, id) {
+    const projectList = getProjectList();
+    let currentProject;
+
+    for (const project of projectList) {
+        if (project.name === projectTitle) {
+            currentProject = project;
+        }
+    }
+    const todoList = currentProject.getTodoList();
+
+    let currentTodo;
+    for (const todo of todoList) {
+        if (todo.id === id) {
+            currentTodo = todo;
+        }
+    }
+
+    if (!currentTodo) return;
+
+    const currentTodoTag = document.querySelector(`.todo[data-id="${currentTodo.id}"]`);
+    const currentOpenTodoTag = document.querySelector(`.open-todo[data-id="${currentTodo.id}"]`);
+
+    if (currentTodoTag) {
+        currentTodoTag.classList.toggle("open-todo");
+        currentTodoTag.classList.toggle("todo");
+
+        renderOpenTodo(currentTodoTag, currentTodo);
+    } else {
+        currentOpenTodoTag.classList.toggle("open-todo");
+        currentOpenTodoTag.classList.toggle("todo");
+
+        renderClosedTodo(currentOpenTodoTag, currentTodo);
+    }
+    
+}
+
+function renderOpenTodo(tag, todo) {
+    tag.textContent = "";
+
+    const completed = todo.completion ? "Completed" : "Not completed";
+
+    const emptyDiv = newElement("div", "", "", tag);
+    newElement("button", "delete-task-btn", "⨯", emptyDiv);
+    const openPair = newElement("div", "open-pair", "", tag);
+    newElement("p", "open-title", todo.title, openPair);
+    newElement("p", "", todo.dueDate, openPair);
+    newElement("p", "open-description", todo.description, tag);
+    const openPriority = newElement("div", "open-priority", "", tag);
+    newElement("p", "", "Priority:", openPriority);
+    newElement("p", "", todo.priority, openPriority);
+    const openCompletion = newElement("div", "open-completion", "", tag);
+    newElement("button", "toggle-btn", "Toggle", openCompletion);
+    newElement("p", "", completed, openCompletion);
+}
+
+function renderClosedTodo(tag, todo) {
+    tag.textContent = "";
+
+    newElement("p", "", todo.title, tag);
+    newElement("p", "", todo.dueDate, tag);
 }
