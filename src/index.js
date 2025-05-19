9c71e79734bd354dca7dcbcc1cf2f7dc4b681136
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-05-19 14:52:44 +0200

    Activate display toggle on add-todo banner

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { addProject, selectProject, handleProject, selectTodo, deleteTodo, toggl
 
 const defaultProject = createProject("Default project");
 addProject(defaultProject.name);
-defaultProject.appendTodo("Sweep floor", "Sweep all the house", "15/07/2025", "Low");
+defaultProject.appendTodo("Sweep floor", "Sweep all house", "15/07/2025", "Low");
 defaultProject.appendTodo("Run", "Run for 4 miles", "17/07/2025", "Mid");
 defaultProject.appendTodo("Groceries", "Buy groceries for the next week", "14/06/2025", "High");
 selectProject(defaultProject.name);
@@ -34,5 +34,12 @@ openProject.addEventListener("click", (e) => {
     } else if (todoElement && openProject.contains(todoElement)) {
         const id = todoElement.dataset.id;
         selectTodo(title, id);
+    } else if (e.target.closest(".add-todo-btn")) {
+        const banner = document.querySelector(".new-todo-banner");
+        if (banner.style.display === 'none') {
+		    banner.style.display = 'block';
+	    } else {
+	    	banner.style.display = 'none';
+	    }
     }
 })
